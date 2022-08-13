20b130c571a4d135304cca98b41d5045857add50
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2022-08-13 15:28:50 +0100

    data managing

--- src/components/results/Results.js
+++ src/components/results/Results.js
@@ -7,7 +7,38 @@ const Container = styled.div`
 `
 
 
-export const Results = () => {
+export const Results = ({data, error}) => {
+    if(!data || error) return null
+
+    //posts = t1
+    //
+
+    console.log(data.data.children)
+    console.log(data.data.children[0])
+    console.log(data.data.children[0].data.subreddit)
+
+
+    const kindConvert = (code) => {
+        switch(code){
+            case "t1":
+                return "comment"
+                break
+            case "t3":
+                return "post"
+                break
+        }
+    }
+
+    data.data.children.forEach(d => {
+        const sub = d.data.subreddit
+
+        const kind = kindConvert(d.kind)
+
+
+        const date = new Date(d.data.created * 1000)
+        console.log(sub,kind, date)
+    })  
+
     return (
         <Container>
             Results
